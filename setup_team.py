3fcf75527abca824bd1ab3283ea5188a47090009
Jeremy-James Noivo <jayjamesnoivo7501@hotmail.com>
2025-12-18 16:23:28 -0500

    Add team setup script

---
+++ setup_team.py
@@ -0,0 +1,13 @@
+def initialiser_projet():
+	print("=== INITIALISATION DU PROJET ===")
+	print("1. Créer dépôt GitHub")
+	print("2. Cloner sur toutes les machines")
+	print("3. Créer branches :")
+	print("   - git checkout -b feature-Jeremy")
+	print("   - git checkout -b feature-Marie-Eve")
+	print("   - git checkout -b feature-Eva")
+	print("   - git checkout -b feature-Jean-Simon")
+	print("4. Commencer à coder !")
+
+if __name__ == "__main__":
+	initialiser_projet()
